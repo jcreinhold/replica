5aca7423b3d5f9d1ad6e81b3d4bfd7eecd947470
Jacob Reinhold <jcreinhold@gmail.com>
2018-04-07 20:08:07 -0400

    moved a parentheses to fix a statement

--- src/utilities/resize3d.m
+++ src/utilities/resize3d.m
@@ -6,7 +6,7 @@ function resized = resize3d(img, target_dim)
     T = affine3d([scale(1) 0 0 0; 0 scale(2) 0 0; 0 0 scale(3) 0; 0 0 0 1]);
     resized = imwarp(img, T, 'cubic');
     if ~all(size(resized) == target_dim)
-        df = floor(abs(target_dim - size(resized)/2));
+        df = floor(abs(target_dim - size(resized))/2);
         resized = resized(df(1):end-df(1),df(2):end-df(2),df(3):end-df(3));
         if ~all(size(resized) == target_dim)
             error(['something went wrong with the resizing, target dim: ' ...
