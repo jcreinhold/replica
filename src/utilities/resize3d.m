bf27ddefdd2eb35f000f3c03d36c44788d00f76b
Jacob Reinhold <jcreinhold@gmail.com>
2018-04-07 20:57:04 -0400

    more resize3d more robust, actually tested it but need to test with real data

--- src/utilities/resize3d.m
+++ src/utilities/resize3d.m
@@ -7,11 +7,13 @@ function resized = resize3d(img, target_dim)
     resized = imwarp(img, T, 'cubic');
     if ~all(size(resized) == target_dim)
         df = abs(target_dim - size(resized));
+        dfm = mod(df,2);
         h = floor(df/2);
-        resized = resized(1+h(1):end-h(1),1+h(2):end-h(2),1+h(3):end-h(3));
-        if any(df >= 10) || ~all(size(resized) == target_dim)
+        resized = resized(1+h(1):end-h(1)-dfm(1),1+h(2):end-h(2)-dfm(2),1+h(3):end-h(3)-dfm(3));
+        if any(df >= 5) || ~all(size(resized) == target_dim)
             error(['something went wrong with the resizing, target dim: ' ...
-                   '%d x %d x %d, resized dim: %d x %d x %d'], target_dim, size(resized))
+                   '%d x %d x %d, resized dim: %d x %d x %d, diff: %d x %d x %d'], ...
+                    target_dim, size(resized), df)
         end
     end
 end
